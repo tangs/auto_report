25f2b5d33ed0634664fe618ba4f33b20590c7e8a
tangs <tangsong1988kaka@gmail.com>
2024-06-12 15:28:43 +0800

    update.

--- lib/rsa/rsa_helper.dart
+++ lib/rsa/rsa_helper.dart
@@ -1,4 +1,6 @@
-import 'package:auto_report/config/config.dart';
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:pointycastle/export.dart';
 
 import 'package:encrypt/encrypt.dart';
@@ -6,8 +8,27 @@ import 'package:encrypt/encrypt.dart';
 class RSAHelper {
   static String encrypt(String plaintext, String publicKey) {
     var parser = RSAKeyParser();
-    final encrypter = Encrypter(
-        RSA(publicKey: parser.parse(Config.rsaPublicKey) as RSAPublicKey));
+    final encrypter =
+        Encrypter(RSA(publicKey: parser.parse(publicKey) as RSAPublicKey));
     return encrypter.encrypt(plaintext).base64;
+  }
+
+  static String decrypt(String plaintext, String privateKey) {
+    final bytes = base64Decode(plaintext);
+    final bytesLen = bytes.length;
+    final key = RSAKeyParser().parse(privateKey) as RSAPrivateKey;
+    final chunkSize = (key.n!.bitLength + 7) ~/ 8;
+    final encrypter = Encrypter(RSA(privateKey: key));
+
+    final sb = StringBuffer();
+    var start = 0;
+    while (start < bytesLen) {
+      final end = min(bytesLen, start + chunkSize);
+      final data = bytes.sublist(start, min(bytesLen, start + chunkSize));
+      start = end;
+      sb.write(encrypter.decrypt(Encrypted(data)));
+    }
+    final ret = sb.toString();
+    return ret;
   }
 }
