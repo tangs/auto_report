8a81cac475494a75db848a665e4ad9aca26d65ec
tangs <tangsong1988kaka@gmail.com>
2024-06-17 17:00:50 +0800

    update window make.

--- make_windows.sh
+++ make_windows.sh
@@ -4,4 +4,6 @@ version=`cat pubspec.yaml | grep 'version:' | awk '{print $2}' | sed 's/+/_/'`
 echo $version
 
 flutter build windows --release
-cp -rf build/windows/x64/runner/Release/auto_report.exe "//192.168.1.248/tangsong/auto_report/windows/auto_report_v$version.app"
+cp -rf build/windows/x64/runner/Release/auto_report.exe "//192.168.1.248/tangsong/auto_report/windows/auto_report_v$version.exe"
+cp -rf build/windows/x64/runner/Release/flutter_windows.dll "//192.168.1.248/tangsong/auto_report/windows/"
+cp -rf build/windows/x64/runner/Release/data "//192.168.1.248/tangsong/auto_report/windows/data"
