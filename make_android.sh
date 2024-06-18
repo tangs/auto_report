0475442f42542ac20a7eaddcc8883654b723ba0f
tangs <tangsong1988kaka@gmail.com>
2024-06-18 18:45:05 +0800

    update.

--- make_android.sh
+++ make_android.sh
@@ -3,6 +3,7 @@
 version=`cat pubspec.yaml | grep 'version:' | awk '{print $2}' | sed 's/+/_/'`
 echo $version
 
+flutter clean
 flutter build apk --release
 
 rm -rf "/Volumes/tangsong/auto_report/android/auto_report_v$version.apk"
