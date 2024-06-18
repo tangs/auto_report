0b2a40bc8f317f2c8b8e6e03547b5a1e4d342e0c
tangs <tangsong1988kaka@gmail.com>
2024-06-18 15:58:10 +0800

    update

--- make_windows.sh
+++ make_windows.sh
@@ -6,6 +6,8 @@ echo $version
 flutter build windows --release
 
 rm -rf "//192.168.1.248/tangsong/auto_report/windows/auto_report_v$version.exe"
+rm -rf "//192.168.1.248/tangsong/auto_report/windows/data"
+rm -rf "//192.168.1.248/tangsong/auto_report/windows/flutter_windows"
 cp -rf build/windows/x64/runner/Release/auto_report.exe "//192.168.1.248/tangsong/auto_report/windows/auto_report_v$version.exe"
 cp -rf build/windows/x64/runner/Release/flutter_windows.dll "//192.168.1.248/tangsong/auto_report/windows/"
 cp -rf build/windows/x64/runner/Release/data "//192.168.1.248/tangsong/auto_report/windows/data"
