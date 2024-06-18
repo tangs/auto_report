903e55885d3bce092fbb0ec1fdfb7006e2a4c09f
tangs <tangsong1988kaka@gmail.com>
2024-06-18 14:30:16 +0800

    update make sh.

--- make_android.sh
+++ make_android.sh
@@ -4,4 +4,6 @@ version=`cat pubspec.yaml | grep 'version:' | awk '{print $2}' | sed 's/+/_/'`
 echo $version
 
 flutter build apk --release
+
+rm -rf "/Volumes/tangsong/auto_report/android/auto_report_v$version.apk"
 cp -rf build/app/outputs/flutter-apk/app-release.apk "/Volumes/tangsong/auto_report/android/auto_report_v$version.apk"
--- make_mac.sh
+++ make_mac.sh
@@ -3,5 +3,7 @@
 version=`cat pubspec.yaml | grep 'version:' | awk '{print $2}' | sed 's/+/_/'`
 echo $version
 
-flutter build apk --release
+flutter build macos --release
+
+rm -rf "/Volumes/tangsong/auto_report/macos/auto_report_v$version.app"
 cp -rf build/macos/Build/Products/Release/auto_report.app "/Volumes/tangsong/auto_report/macos/auto_report_v$version.app"
