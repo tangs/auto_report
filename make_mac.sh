e16bbd18595476ba5e19d8abde30be134703fd3b
tangs <tangsong1988kaka@gmail.com>
2024-07-15 17:58:18 +0800

    update make mac sh.

--- make_mac.sh
+++ make_mac.sh
@@ -3,17 +3,24 @@
 version=`cat pubspec.yaml | grep 'version:' | awk '{print $2}' | sed 's/+/_/'`
 echo $version
 
+buildPath='release/macos'
+mkdir -p $buildPath
+
 sh setup_kbz.sh
 flutter clean
 flutter build macos --release
 
-rm -rf "/Volumes/tangsong/auto_report/macos/kbz_reporter_v$version.app"
-cp -rf build/macos/Build/Products/Release/auto_report.app "/Volumes/tangsong/auto_report/macos/kbz_reporter_v$version.app"
+rm -rf "$buildPath/kbz_reporter_v$version.app"
+rm -rf "$buildPath/auto_report.app"
+cp -rf build/macos/Build/Products/Release/auto_report.app "$buildPath"
+mv "$buildPath/auto_report.app" "$buildPath/kbz_reporter_v$version.app"
 
 
 sh setup_wave.sh
 flutter clean
 flutter build macos --release
 
-rm -rf "/Volumes/tangsong/auto_report/macos/wave_reporter_v$version.app"
-cp -rf build/macos/Build/Products/Release/auto_report.app "/Volumes/tangsong/auto_report/macos/wave_reporter_v$version.app"
+rm -rf "$buildPath/wave_reporter_v$version.app"
+rm -rf "$buildPath/auto_report.app"
+cp -rf build/macos/Build/Products/Release/auto_report.app "$buildPath"
+mv "$buildPath/auto_report.app" "$buildPath/wave_reporter_v$version.app"
