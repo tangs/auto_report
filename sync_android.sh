f7af880d6d0e69574a57bbacc349719710d981ce
tangs <tangsong1988kaka@gmail.com>
2024-09-19 17:56:55 +0800

    update android sync sh.

--- sync_android.sh
+++ sync_android.sh
@@ -8,6 +8,8 @@ destPath='/Volumes/tangsong/auto_report/android'
 
 rm -rf "$destPath/kbz_reporter_v$version.apk"
 rm -rf "$destPath/wave_reporter_v$version.apk"
+rm -rf "$destPath/kbiz_reporter_v$version.apk"
 
 cp -rf "$buildPath/kbz_reporter_v$version.apk" "$destPath/"
 cp -rf "$buildPath/wave_reporter_v$version.apk" "$destPath/"
+cp -rf "$buildPath/kbiz_reporter_v$version.apk" "$destPath/"
