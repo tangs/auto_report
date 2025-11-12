27b6b917167d733cdb86c5c80f261fdbcf4f853a
tangs <tangsong1988kaka@gmail.com>
2025-11-12 11:54:19 +0800

    update kbz.

--- lib/config/global_config.dart
+++ lib/config/global_config.dart
@@ -1,7 +1,7 @@
 import 'package:auto_report/widges/bank_selector.dart';
 
 class GlobalConfig {
-  static BankType bankType = BankType.wave;
+  static BankType bankType = BankType.kbz;
 
   static const logCountMax = 1024;
 
